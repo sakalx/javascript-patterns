0598833593355939b5b03a84b55ef2b67d39fc80
sakal <sakals@mail.ua>
2018-09-09 23:18:13 -0400

    init: loader UI
    Signed-off-by: sakal <sakals@mail.ua>

--- src/components/lazy-load/index.js
+++ src/components/lazy-load/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 
+import LinearProgress from '@material-ui/core/LinearProgress';
+
 const LazyLoad = opts => Loadable(Object.assign({
-  loading: () => <span>Loading</span>
+  loading: () => <LinearProgress
+    style={{
+      position: 'fixed',
+      width: '100%',
+      zIndex: 2000,
+      top: 0,
+      left: 0,
+      height: 3,
+    }}
+  />,
+  delay: 66,
 }, opts));
-// delay 66
 export default LazyLoad
